f57a7509ef81dd0fd5c3751c5be8a357aa964739
Lars K <lars195@yahoo.de>
2013-09-30 17:01:43 +0200

    Started work on a simple Plugin-System.

---
+++ src/l19/pluginSys/PluginBase.java
@@ -0,0 +1,17 @@
+package l19.pluginSys;
+
+public class PluginBase {
+	
+	public PluginBase(){
+		
+	}
+	
+	/**
+	 * Init method.
+	 * This method is called on application startup by the plugin-loader.
+	 **/
+	public void init(){
+		
+	}
+	
+}
---
+++ src/l19/pluginSys/PluginManager.java
@@ -0,0 +1,17 @@
+package l19.pluginSys;
+
+import java.io.File;
+
+import heroesgrave.paint.main.Paint;
+
+public class PluginManager {
+	private Paint appInstance;
+	private File pluginRootDirectory;
+	
+	public PluginManager(Paint paint){
+		
+		
+		
+	}
+	
+}
