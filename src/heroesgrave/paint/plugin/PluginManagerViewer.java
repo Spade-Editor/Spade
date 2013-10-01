a284d312e86c2b1b8a77f8714e92039dfc827683
Lars K <lars195@yahoo.de>
2013-10-01 15:54:30 +0200

    Created simple Plugin-Info system.

--- src/heroesgrave/paint/plugin/PluginManagerViewer.java
+++ src/heroesgrave/paint/plugin/PluginManagerViewer.java
@@ -11,6 +11,7 @@ import heroesgrave.paint.gui.Menu.CentredJDialog;
 import javax.swing.JDialog;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.ListModel;
 import javax.swing.WindowConstants;
 import javax.swing.event.ListDataListener;
@@ -55,20 +56,32 @@ public class PluginManagerViewer {
 		public String toString(){
 			return plugin.name;
 		}
+
+		public void constructText(JTextArea pluginInfoArea) {
+			pluginInfoArea.append("Plugin Name: " + plugin.name + "\n");
+			
+			
+		}
 		
 	}
 	
+	
+	JDialog dialog = null;
+	JScrollPane listScrollPane = null;
+	JList<PluginListItem> list = null;
+	PluginListModel model = null;
+	JTextArea pluginInfoArea = null;
+	
 	public void show(PluginManager pluginManager) {
-		final JDialog dialog = new CentredJDialog();
+		dialog = new CentredJDialog();
 		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		dialog.setTitle("Plugin Manager");
-		dialog.setSize(400, 300);
-		dialog.setPreferredSize(new Dimension(400,300));
+		dialog.setSize(640, 480);
+		dialog.setPreferredSize(new Dimension(640, 480));
 		dialog.setLayout(new BorderLayout());
 		
-		JScrollPane listScrollPane = new JScrollPane();
-		JList<PluginListItem> list = new JList<PluginListItem>();
-		PluginListModel model = new PluginListModel();
+		list = new JList<PluginListItem>();
+		model = new PluginListModel();
 		list.setModel(model);
 		model.addAll(pluginManager.getPluginList());
 		list.revalidate();
@@ -77,19 +90,36 @@ public class PluginManagerViewer {
 			public void mouseClicked(MouseEvent evt) {
 		    	// We absolutely know that the source is our plugin-list, so we can safely cast.
 		        JList<PluginListItem> list = (JList<PluginListItem>)evt.getSource();
-		        if (evt.getClickCount() == 2) {
+		        
+		        //if (evt.getClickCount() == 2)
+		        {
 		            int index = list.locationToIndex(evt.getPoint());
+		            PluginListModel dlm = (PluginListModel) list.getModel();
+		            PluginListItem item = dlm.getElementAt(index);;
+		            list.ensureIndexIsVisible(index);
 		            
-		            System.out.println("Clicked Element: " + index);
+		            // Clear the TextArea so the plugin-info-element can write the info...
+		            pluginInfoArea.setText("");
+		            item.constructText(pluginInfoArea);
+		            
+		            // Set the info visible
+		    		pluginInfoArea.setVisible(true);
+		            
 		        }
 		    }
 		});
 		
+		listScrollPane = new JScrollPane();
 		listScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		listScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		listScrollPane.setViewportView(list);
-		
 		dialog.add(listScrollPane, BorderLayout.CENTER);
+		
+		pluginInfoArea = new JTextArea();
+		pluginInfoArea.setText("Nothing in here...");
+		pluginInfoArea.setEditable(false);
+		pluginInfoArea.setVisible(false);
+		dialog.add(pluginInfoArea, BorderLayout.PAGE_END);
 		
 		dialog.pack();
 		dialog.setResizable(true);
