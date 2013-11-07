26b7bf933f57dc90a6e5431b0d25707c24e03c52
Lars K <lars195@yahoo.de>
2013-11-07 20:53:09 +0100

    (added a note and corrected a tiny mistake)

--- src/heroesgrave/paint/imageops/RotateRightBy90.java
+++ src/heroesgrave/paint/imageops/RotateRightBy90.java
@@ -27,7 +27,32 @@ public class RotateRightBy90 extends ImageOp
         at.rotate(Math.toRadians(90));
         
         g2d.drawImage(old, at, null);
-		
+		g2d.dispose();
+        
 		Paint.addChange(new ImageChange(newImage));
 	}
+	
+	/**
+	 * More advanced method from Stackoverflow:
+
+	public static BufferedImage rotate(BufferedImage image, double angle) {
+      double sin = Math.abs(Math.sin(angle)), cos = Math.abs(Math.cos(angle));
+      int w = image.getWidth(), h = image.getHeight();
+      int neww = (int)Math.floor(w*cos+h*sin), newh = (int)Math.floor(h*cos+w*sin);
+      
+      GraphicsConfiguration gc = getDefaultConfiguration();
+      BufferedImage result = gc.createCompatibleImage(neww, newh, Transparency.TRANSLUCENT);
+      
+      Graphics2D g = result.createGraphics();
+      g.translate((neww-w)/2, (newh-h)/2);
+      g.rotate(angle, w/2, h/2);
+      g.drawRenderedImage(image, null);
+      g.dispose();
+      
+      return result;
+}
+
+	 * 
+	 **/
+	
 }
