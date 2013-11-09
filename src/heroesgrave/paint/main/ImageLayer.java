e7dfaf58fa829454c221f08b373a89dca5827fec
Lars K <lars195@yahoo.de>
2013-11-09 17:12:47 +0100

    (how-it-could-look-like)
    This is just how the 'ImageLayer'-class could basically be like. This is
    by no means an actual implementation, just a bit of tinkering around.

---
+++ src/heroesgrave/paint/main/ImageLayer.java
@@ -0,0 +1,14 @@
+package heroesgrave.paint.main;
+
+import java.awt.image.BufferedImage;
+
+public class ImageLayer {
+	
+	String name;
+	
+	// BlendMode blending;
+	
+	BufferedImage image;
+	
+	
+}
