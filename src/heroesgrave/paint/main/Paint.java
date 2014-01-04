1ddd4a4b58ff47399a39808e829270f56be7d3d7
HeroesGrave <heroesgrave@gmail.com>
2014-01-04 15:12:00 +1300

    changed version and updated build

--- src/heroesgrave/paint/main/Paint.java
+++ src/heroesgrave/paint/main/Paint.java
@@ -56,16 +56,14 @@ public class Paint extends Application
 	// Eg: 1.3b is the 2nd revision of verion 1.3
 	
 	// Stable for stable post-1.0 versions
-	// Beta for all stable versions pre-1.0
-	// Alpha for new completed features.
+	// Beta for new completed features.
 	// Development for under-development new features.
 	
-	public static final String VERSION = "0.9c";
+	public static final String VERSION = "1.0-Dev";
 	public static final String RELEASED = "04/01/2013";
 	
-	//public static final String BUILD_TYPE = "Development";
-	//public static final String BUILD_TYPE = "Alpha";
-	public static final String BUILD_TYPE = "Beta";
+	public static final String BUILD_TYPE = "Development";
+	//public static final String BUILD_TYPE = "Beta";
 	//public static final String BUILD_TYPE = "Stable";
 	
 	public static boolean debug;
