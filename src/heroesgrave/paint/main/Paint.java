ff15238f40c109de7881d717e6f3651a8f1e3afd
HeroesGrave <heroesgrave@gmail.com>
2014-01-30 17:47:39 +1300

    frogot to update build. grrrr

--- src/heroesgrave/paint/main/Paint.java
+++ src/heroesgrave/paint/main/Paint.java
@@ -59,7 +59,7 @@ public class Paint extends Application
 	// Beta for new completed features.
 	// Development for under-development new features.
 	
-	public static final String VERSION = "0.12";
+	public static final String VERSION = "0.12a";
 	public static final String RELEASED = "30/01/2014";
 	
 	/*/public static final String BUILD_TYPE = "Development";
