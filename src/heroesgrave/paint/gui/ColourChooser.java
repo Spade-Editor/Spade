5cc2e143cd757270babd312247bd119f3284c4fc
Josh <xburntpizza@gmail.com>
2014-02-13 02:06:57 -0500

    Quick cleanup, ready for merge of #59

--- src/heroesgrave/paint/gui/ColourChooser.java
+++ src/heroesgrave/paint/gui/ColourChooser.java
@@ -1053,6 +1053,7 @@ public class ColourChooser
 		@Override
 		public void mousePressed(MouseEvent e) {
 			button = e.getButton();
+			mouseReleased(e);
 		}
 		@Override
 		public void mouseEntered(MouseEvent e) {
@@ -1760,49 +1761,19 @@ public class ColourChooser
 	private static int sliderLastChanged = -1;
 	
 	/*
-	 * Right now just a copy of Color.RGBtoHSB(), figuring out how to eliminate some oddities
+	 * Tweak of Color.RGBtoHSB()
+	 * Doesn't lock hue if saturation or brightness is zero, and doesn't lock saturation if brightness is zero.
 	 */
 	private float[] RGBtoHSB(int r, int g, int b, float[] hsbvals) {
-		float hue, saturation, brightness;
-        if(hsbvals == null)
-        	hsbvals = new float[3];
-        int cmax = (r > g) ? r : g;
-        if (b > cmax) cmax = b;
-        int cmin = (r < g) ? r : g;
-        if (b < cmin) cmin = b;
-
-        brightness = ((float) cmax) / 255.0f;
-        if (cmax != 0)
-            saturation = ((float) (cmax - cmin)) / ((float) cmax);
-        else
-        	saturation = 0;
-        if (saturation == 0)
-            hue = 0;
-        else {
-            float redc = ((float) (cmax - r)) / ((float) (cmax - cmin));
-            float greenc = ((float) (cmax - g)) / ((float) (cmax - cmin));
-            float bluec = ((float) (cmax - b)) / ((float) (cmax - cmin));
-            if (r == cmax)
-                hue = bluec - greenc;
-            else if (g == cmax)
-                hue = 2.0f + redc - bluec;
-            else
-                hue = 4.0f + greenc - redc;
-            hue = hue / 6.0f;
-            if (hue < 0)
-                hue = hue + 1.0f;
-        }
+        hsbvals = Color.RGBtoHSB(r, g, b, hsbvals);
         
         if(sliderLastChanged == 1)
-        	hue = chooserRightHSBimplH.sliderValue;
+        	hsbvals[0] = chooserRightHSBimplH.sliderValue;
         if(sliderLastChanged == 2) {
-        	hue = chooserRightHSBimplH.sliderValue;
-        	saturation = chooserRightHSBimplS.sliderValue;
+        	hsbvals[0] = chooserRightHSBimplH.sliderValue;
+        	hsbvals[1] = chooserRightHSBimplS.sliderValue;
         }
-        
-        hsbvals[0] = hue;
-        hsbvals[1] = saturation;
-        hsbvals[2] = brightness;
+
         return hsbvals;
 	}
 	
