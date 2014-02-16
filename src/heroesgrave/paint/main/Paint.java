1d182d8c537b31aa16190a1a7e5c66490aa454d4
HeroesGrave <heroesgrave@gmail.com>
2014-02-16 15:12:45 +1300

    1.0-Beta.1

--- src/heroesgrave/paint/main/Paint.java
+++ src/heroesgrave/paint/main/Paint.java
@@ -60,10 +60,10 @@ public class Paint extends Application
 	// Beta for new completed features.
 	// Development for under-development new features.
 	
-	public static final String VERSION = "1.0-Dev";
-	public static final String RELEASED = "14/02/2014";
+	public static final String VERSION = "1.0-Beta.1";
+	public static final String RELEASED = "16/02/2014";
 	
-	/**/public static final String BUILD_TYPE = "Development";
+	/*/public static final String BUILD_TYPE = "Development";
 	//*/public static final String BUILD_TYPE = "Beta";
 	//*/public static final String BUILD_TYPE = "Stable";
 	
