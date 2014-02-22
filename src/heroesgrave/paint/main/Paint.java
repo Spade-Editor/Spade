7954ecc6bca5f90717864eff89537d8c4acbca98
HeroesGrave <heroesgrave@gmail.com>
2014-02-23 12:45:43 +1300

    I hate git

--- src/heroesgrave/paint/main/Paint.java
+++ src/heroesgrave/paint/main/Paint.java
@@ -61,7 +61,7 @@ public class Paint extends Application
 	// Development for under-development new features.
 	
 	public static final String VERSION = "1.0-Beta.5";
-	public static final String RELEASED = "20/02/2014";
+	public static final String RELEASED = "23/02/2014";
 	
 	/*/public static final String BUILD_TYPE = "Development";
 	//*/public static final String BUILD_TYPE = "Beta";
