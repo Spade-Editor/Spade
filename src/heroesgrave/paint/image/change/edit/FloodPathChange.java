c1ae844e0b65a84faad013d7882c717011f7085c
BurntPizza <xburntpizza@gmail.com>
2014-11-04 18:59:59 -0500

    Tweaked flood fill + some doc

--- src/heroesgrave/paint/image/change/edit/FloodPathChange.java
+++ src/heroesgrave/paint/image/change/edit/FloodPathChange.java
@@ -6,12 +6,24 @@ import java.awt.Point;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+/**
+ * Same as a PathChange (a series of points), but flood-fills the image starting at each point.
+ * 
+ * @author BurntPizza
+ *
+ */
 public class FloodPathChange extends PathChange
 {
 
 	public FloodPathChange(Point p, int colour)
 	{
 		super(p, colour);
+		
+	}
+	
+	public FloodPathChange(short x, short y, int colour)
+	{
+		super(x, y, colour);
 		
 	}
 	
@@ -25,6 +37,7 @@ public class FloodPathChange extends PathChange
 	}
 	
 	// XXX: move to RawImage instance method
+	// scan line flood fill as described in Wikipedia
 	private static void floodFill(RawImage image, int x, int y, int color)
 	{
 		if (x < 0 | y < 0 || x >= image.width || y >= image.height)
@@ -37,24 +50,25 @@ public class FloodPathChange extends PathChange
 		if (targetColor == color || (mask != null && !mask[x + y * image.width]))
 			return;
 		
+		// if more speed is necessary, implement an IntQueue
 		Queue<Integer> queue = new ArrayDeque<>();
 		
 		queue.add(x + y * image.width);
-		
+
 		while(!queue.isEmpty())
 		{
 			int n = queue.poll();
-			if(buffer[n] == targetColor && (mask == null || mask[n]))
+			if(buffer[n] == targetColor) // mask predicate is guaranteed here
 			{
 				int ny = n / image.height;
 				int nyo = ny * image.height;
-				int nx = n % image.width;
-				int w = nx, e = nx;
-				while(w >= 0 && buffer[w + nyo] == targetColor && (mask == null || mask[w + nyo])) --w;
-				while(e < image.width && buffer[e + nyo] == targetColor && (mask == null || mask[e + nyo])) ++e;
-				w += 1 + nyo;
-				e += nyo;
-				for(int i = w; i < e; ++i)
+				int nxl = nyo + image.width;
+				int w = n, e = n;
+				// scan out on each side of current pixel
+				while(w >= nyo && buffer[w] == targetColor && (mask == null || mask[w])) --w;
+				while(e < nxl && buffer[e] == targetColor && (mask == null || mask[e])) ++e;
+				// fill in between
+				for(int i = w + 1; i < e; ++i)
 				{
 					buffer[i] = color;
 					if (ny > 0 && buffer[i - image.height] == targetColor && (mask == null || mask[i - image.height]))
