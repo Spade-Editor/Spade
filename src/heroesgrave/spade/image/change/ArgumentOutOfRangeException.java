61f68dea1a69cc013f062130a5b37b9eaa235427
Nick <yux50000@hotmail.com>
2015-01-01 02:07:01 -0500

    Added a couple of things that are somewhat necessary. The getArrayIndex method simply provides a public way to access the index method of RawImages; It's quite useful when writing effects and editing the pixel buffer.

---
+++ src/heroesgrave/spade/image/change/ArgumentOutOfRangeException.java
@@ -0,0 +1,37 @@
+package heroesgrave.spade.image.change;
+
+@SuppressWarnings("serial")
+public class ArgumentOutOfRangeException extends RuntimeException
+{
+
+	public ArgumentOutOfRangeException()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArgumentOutOfRangeException(String message)
+	{
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArgumentOutOfRangeException(Throwable cause)
+	{
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArgumentOutOfRangeException(String message, Throwable cause)
+	{
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArgumentOutOfRangeException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace)
+	{
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
